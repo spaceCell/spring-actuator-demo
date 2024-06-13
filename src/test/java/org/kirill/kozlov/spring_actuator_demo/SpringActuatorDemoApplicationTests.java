d82ee0f799e2ae920b7fe2a35c3fe914013d922e
Kirill Kozlov <space_cell@proton.me>
2024-06-13 11:33:17 +0500

    Init project

---
+++ src/test/java/org/kirill/kozlov/spring_actuator_demo/SpringActuatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kirill.kozlov.spring_actuator_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
