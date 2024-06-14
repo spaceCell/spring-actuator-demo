d42c590d0e1d06c8764866dd765b0bd7471b8771
Kirill Kozlov <space_cell@proton.me>
2024-06-14 10:10:33 +0500

    add droid controller

---
+++ src/main/java/org/kirill/kozlov/spring_actuator_demo/entity/Droid.java
@@ -0,0 +1,10 @@
+package org.kirill.kozlov.spring_actuator_demo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Droid {
+    private String id, description;
+}
